1564dcee0098842dd8db80c9f48e91a3a0bda826
Sam Vevang <sam.vevang@gmail.com>
2018-02-28 11:36:42 -0600

    rename wiki_extractor_tokenizer -> tokenizer_scheduler

--- lib/mass_tokenize.ex
+++ lib/mass_tokenize.ex
@@ -16,50 +16,50 @@ defmodule MassTokenize do
     |> IO.write
   end
 
-  def finished?(file_reader_scheduler, wiki_extractor_tokenizer) do
-    InteractingScheduler.queue_drained?(file_reader_scheduler) and InteractingScheduler.queue_drained?(wiki_extractor_tokenizer)
+  def finished?(file_reader_scheduler, tokenizer_scheduler) do
+    InteractingScheduler.queue_drained?(file_reader_scheduler) and InteractingScheduler.queue_drained?(tokenizer_scheduler)
   end
 
-  def run_queues(file_reader_scheduler, wiki_extractor_tokenizer) do
+  def run_queues(file_reader_scheduler, tokenizer_scheduler) do
 
-    if finished?(file_reader_scheduler, wiki_extractor_tokenizer) do
+    if finished?(file_reader_scheduler, tokenizer_scheduler) do
       # XXX fixme, wait for the printer tasks to finish
       :timer.sleep(1000)
       exit(:normal)
     end
 
-    if length(wiki_extractor_tokenizer.queue) < 16 do
+    if length(tokenizer_scheduler.queue) < 16 do
       file_reader_scheduler = InteractingScheduler.schedule_processes(file_reader_scheduler)
     end
-    wiki_extractor_tokenizer = InteractingScheduler.schedule_processes(wiki_extractor_tokenizer)
+    tokenizer_scheduler = InteractingScheduler.schedule_processes(tokenizer_scheduler)
 
     file_reader_uid = file_reader_scheduler.uid
-    wiki_extractor_uid = wiki_extractor_tokenizer.uid
+    wiki_extractor_uid = tokenizer_scheduler.uid
 
     receive do
       {:answer, ^file_reader_uid, result, worker_pid} ->
         file_reader_scheduler = InteractingScheduler.receive_answer(file_reader_scheduler, result, worker_pid)
         Logger.debug("[MassTokenize] answer from FileReader #{file_reader_uid}")
-        wiki_extractor_tokenizer = InteractingScheduler.push_queue(wiki_extractor_tokenizer, result)
-        run_queues(file_reader_scheduler, wiki_extractor_tokenizer)
+        tokenizer_scheduler = InteractingScheduler.push_queue(tokenizer_scheduler, result)
+        run_queues(file_reader_scheduler, tokenizer_scheduler)
       {:answer, ^wiki_extractor_uid, result, worker_pid} ->
-        wiki_extractor_tokenizer = InteractingScheduler.receive_answer(wiki_extractor_tokenizer, result, worker_pid)
+        tokenizer_scheduler = InteractingScheduler.receive_answer(tokenizer_scheduler, result, worker_pid)
         Task.start_link(fn -> print_result_list(result) end)
         Logger.debug("[MassTokenize] answer from TokenizeWikiExtractorJson #{wiki_extractor_uid}")
-        run_queues(file_reader_scheduler, wiki_extractor_tokenizer)
+        run_queues(file_reader_scheduler, tokenizer_scheduler)
       anything ->
         Logger.debug("[MassTokenize] received #{inspect(anything)}")
-        run_queues(file_reader_scheduler, wiki_extractor_tokenizer)
+        run_queues(file_reader_scheduler, tokenizer_scheduler)
     end
 
   end
 
   def start_tokenizer_scheduler(wikiextractor_json: false) do
-    wiki_extractor_tokenizer = InteractingScheduler.run(self(), @num_tok_workers, TokenizeText, :parse_string, [])
+    tokenizer_scheduler = InteractingScheduler.run(self(), @num_tok_workers, TokenizeText, :parse_string, [])
   end
 
   def start_tokenizer_scheduler(wikiextractor_json: true) do
-    wiki_extractor_tokenizer = InteractingScheduler.run(self(), @num_tok_workers, TokenizeWikiExtractorJson, :parse_string, [])
+    tokenizer_scheduler = InteractingScheduler.run(self(), @num_tok_workers, TokenizeWikiExtractorJson, :parse_string, [])
   end
 
   def tokenize_text_files([path: dir, wikiextractor_json: json_lines]) do
@@ -68,9 +68,9 @@ defmodule MassTokenize do
     |> List.flatten
 
     file_reader_scheduler = InteractingScheduler.run(self(), @num_file_workers, FileReader, :read_text, file_list)
-    wiki_extractor_tokenizer = start_tokenizer_scheduler(wikiextractor_json: json_lines)
+    tokenizer_scheduler = start_tokenizer_scheduler(wikiextractor_json: json_lines)
 
-    run_queues(file_reader_scheduler, wiki_extractor_tokenizer)
+    run_queues(file_reader_scheduler, tokenizer_scheduler)
   end
 
   def gather_tree(dir, results) do
